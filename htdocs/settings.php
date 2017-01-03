f48982b7a477a86a2f8bfefa7e671d70761331d4
andrii <diachenkoandrii7@gmail.com>
2017-01-03 20:51:14 +0200

    user2

---
+++ htdocs/settings.php
@@ -0,0 +1,16 @@
+<?php
+	echo "Hello Git";
+	echo "Hello Git2";
+
+<<<<<<< HEAD
+	echo "Hello, world!<br>";
+            echo "2 + 5 = ";
+            echo (2+5);
+?>
+=======
+	echo "Branch Master";
+>>>>>>> master
+
+
+	echo " push + commit";
+	echo " new_git+settings";
